c21830daff89e1e5fa27ccb88bbc4ec2c5ba2a90
Rgstaub <rgstaub@gmail.com>
2017-06-23 01:00:50 -0400

    yuge progress

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+"use strict";
 /*	Pseudo code
 	
 	Characters are objects with stats like hp, attack, defense, and counter-attack
@@ -29,49 +30,49 @@
 // var characterStats = ["knight", "enemy"];
 
 
-var knightStats = ["Bast", "knight", 250, 2, 50, 20, false, "knight.png"];
-var mageStats = ["Denna", "mage", 100, 4, 25, 40, true, "mage.png"];
-var priestStats = ["Auri", "priest", 130, 3, 15, 50, true, "priest.png"];
-var monkStats = ["Tempi", "monk", 160, 3, 45, 45, false, "monk.png"];
-var slimeStats = ["Slime", "enemy", 80, 2, 10, 20, false];
-var trollStats = ["Troll", "enemy", 150, 4, 15, 35, false];
+// var knightStats = ["Bast", "knight", 250, 2, 50, 20, false, "knight.png"];
+// var mageStats = ["Denna", "mage", 100, 4, 25, 40, true, "mage.png"];
+// var priestStats = ["Auri", "priest", 130, 3, 15, 50, true, "priest.png"];
+// var monkStats = ["Tempi", "monk", 160, 3, 45, 45, false, "monk.png"];
+// var slimeStats = ["Slime", "enemy", 80, 2, 10, 20, false];
+// var trollStats = ["Troll", "enemy", 150, 4, 15, 35, false];
 
-function Hero(stats) {
-	var that = this;
-	this.name = stats[0];
-	this.charClass = stats[1];
-	this.hp = stats[2];
-	this.baseAttk = stats[3];
-	this.def = stats[4];
-	this.counterAttack = stats[5];
-	this.range = stats[6];
-	this.position = "teamCamp";
-	this.divId = this.charClass + "Div";
-	this.img = "<div class='col-xs-3 hero' id='" + this.charClass + "Div'><img id='" + this.charClass + "Image' src='assets/images/" + this.charClass + ".png' alt='" + this.charClass + ".png'></div>";
-	var image = this.img;
+// function Hero(stats) {
+// 	var that = this;
+// 	this.name = stats[0];
+// 	this.charClass = stats[1];
+// 	this.hp = stats[2];
+// 	this.baseAttk = stats[3];
+// 	this.def = stats[4];
+// 	this.counterAttack = stats[5];
+// 	this.range = stats[6];
+// 	this.position = "teamCamp";
+// 	this.divId = this.charClass + "Div";
+// 	this.img = "<div class='col-xs-3 hero' id='" + this.charClass + "Div'><img id='" + this.charClass + "Image' src='assets/images/" + this.charClass + ".png' alt='" + this.charClass + ".png'></div>";
+// 	var image = this.img;
 
-	this.toCamp = function() {
-		relocate(this.divId);
-		console.log(this.divId);
-		$("#teamCamp").append(this.img);
-		this.position = $("#teamCamp");
-	}
-	this.toBattle = function() {
-		relocate(this.divId);
-		console.log(this.divId);
-		$("#battlefield").append(this.img);
-		this.position = $("#battlefield");
-	}
-	this.initialize = function() {
-		$("#teamCamp").append(this.img);
-		$(".hero").click(function() {
-			console.log(this);
-			console.log(that);
-		// 	relocate(that);
+// 	this.toCamp = function() {
+// 		relocate(this.divId);
+// 		console.log(this.divId);
+// 		$("#teamCamp").append(this.img);
+// 		this.position = $("#teamCamp");
+// 	}
+// 	this.toBattle = function() {
+// 		relocate(this.divId);
+// 		console.log(this.divId);
+// 		$("#battlefield").append(this.img);
+// 		this.position = $("#battlefield");
+// 	}
+// 	this.initialize = function() {
+// 		$("#teamCamp").append(this.img);
+// 		$(".hero").click(function() {
+// 			console.log(this);
+// 			console.log(that);
+// 		// 	relocate(that);
 
-		});
-	}
-}
+// 		});
+// 	}
+// }
 
 // function relocate(hero) {
 // 	console.log(hero);
@@ -89,17 +90,17 @@ function Hero(stats) {
 // 	else {
 		
 // 	}
-// }
+//
 
-var mage = new Hero(mageStats);
-var knight = new Hero(knightStats);
-var priest = new Hero(priestStats);
-var monk = new Hero(monkStats);
+// var mage = new Hero(mageStats);
+// var knight = new Hero(knightStats);
+// var priest = new Hero(priestStats);
+// var monk = new Hero(monkStats);
 
-knight.initialize();
-mage.initialize();
-priest.initialize();
-monk.initialize();
+// knight.initialize();
+// mage.initialize();
+// priest.initialize();
+// monk.initialize();
 
 // $(".hero").click(function() {
 
@@ -112,3 +113,65 @@ monk.initialize();
 // 			// relocate(that);
 
 // });
+// var knightStats = ["Bast", "knight", 250, 2, 50, 20, false, "knight.png"];
+// var mageStats = ["Denna", "mage", 100, 4, 25, 40, true, "mage.png"];
+
+var heroes = {
+	"knight": {
+		"hp": 250,
+		"attk": 4,
+		"def": 75,
+		"counterAttack": 20,
+		
+	},
+	"mage": {
+		"hp": 100,
+		"attk": 15,
+		"def": 25,
+		"counterAttack": 40,
+		
+	},
+}
+
+var enemies = {
+	"minataur": {
+		"hp": 250,
+		"attk": 4,
+		"def": 75,
+		"counterAttack": 20,
+	},
+	"warlock": {
+		"hp": 250,
+		"attk": 4,
+		"def": 75,
+		"counterAttack": 20,	
+	},
+}
+
+$("img").on("click", function() {
+	move(this);
+	var clickedHero = $(this).attr("data-hero");
+	var currHero = heroes[clickedHero];
+	// $(this).attr("data-location", "battle");
+
+	
+})
+
+function move(image) {
+	if ($(image).attr("data-location") === "camp") {
+		console.log("move from camp to battlefield");
+		$("#battlefield").append(image);
+		$(image).attr("data-location", "battle");
+		console.log($(image).attr("data-location"));
+		$(image).removeClass("col-xs-3").addClass("col-xs-10 col-xs-offset-2");
+	}
+	else if ($(image).attr("data-location") === "battle") {
+		console.log("move from battlefield to camp");
+		$("#teamCamp").append(image);
+		$(image).attr("data-location", "camp");
+		$(image).removeClass("col-xs-10 col-xs-offset-2").addClass("col-xs-3");
+	}
+	else {
+		alert("fail");
+	}
+}
