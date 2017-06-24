0c9bbc4d1b322100cfee16246fb96d28e32fb7a9
Rgstaub <rgstaub@gmail.com>
2017-06-23 23:05:04 -0400

    running into some trouble. considering going back to strict instructions for simplicity

--- assets/js/game.js
+++ assets/js/game.js
@@ -26,105 +26,17 @@
 	Bonus - popover to display all character stats
 */
 
-// Declare the pre-built character starting stat sets using a multi-dimensional array
-// var characterStats = ["knight", "enemy"];
 
-
-// var knightStats = ["Bast", "knight", 250, 2, 50, 20, false, "knight.png"];
-// var mageStats = ["Denna", "mage", 100, 4, 25, 40, true, "mage.png"];
-// var priestStats = ["Auri", "priest", 130, 3, 15, 50, true, "priest.png"];
-// var monkStats = ["Tempi", "monk", 160, 3, 45, 45, false, "monk.png"];
-// var slimeStats = ["Slime", "enemy", 80, 2, 10, 20, false];
-// var trollStats = ["Troll", "enemy", 150, 4, 15, 35, false];
-
-// function Hero(stats) {
-// 	var that = this;
-// 	this.name = stats[0];
-// 	this.charClass = stats[1];
-// 	this.hp = stats[2];
-// 	this.baseAttk = stats[3];
-// 	this.def = stats[4];
-// 	this.counterAttack = stats[5];
-// 	this.range = stats[6];
-// 	this.position = "teamCamp";
-// 	this.divId = this.charClass + "Div";
-// 	this.img = "<div class='col-xs-3 hero' id='" + this.charClass + "Div'><img id='" + this.charClass + "Image' src='assets/images/" + this.charClass + ".png' alt='" + this.charClass + ".png'></div>";
-// 	var image = this.img;
-
-// 	this.toCamp = function() {
-// 		relocate(this.divId);
-// 		console.log(this.divId);
-// 		$("#teamCamp").append(this.img);
-// 		this.position = $("#teamCamp");
-// 	}
-// 	this.toBattle = function() {
-// 		relocate(this.divId);
-// 		console.log(this.divId);
-// 		$("#battlefield").append(this.img);
-// 		this.position = $("#battlefield");
-// 	}
-// 	this.initialize = function() {
-// 		$("#teamCamp").append(this.img);
-// 		$(".hero").click(function() {
-// 			console.log(this);
-// 			console.log(that);
-// 		// 	relocate(that);
-
-// 		});
-// 	}
-// }
-
-// function relocate(hero) {
-// 	console.log(hero);
-// 	console.log(hero.position);
-// 	if (hero.position === "teamCamp") {
-// 		console.log("to battle. currently at " + hero.position);
-// 		console.log(hero.divId);
-// 		$("'#" + hero.divId + "'").remove();
-// 		$("#battlefield").append(hero.img);
-
-// 	}
-// 	else if (hero.position === "battlefield") {
-		
-// 	}
-// 	else {
-		
-// 	}
-//
-
-// var mage = new Hero(mageStats);
-// var knight = new Hero(knightStats);
-// var priest = new Hero(priestStats);
-// var monk = new Hero(monkStats);
-
-// knight.initialize();
-// mage.initialize();
-// priest.initialize();
-// monk.initialize();
-
-// $(".hero").click(function() {
-
-// 	console.log(this);
-
-
-
-// 			// console.log(this);
-// 			// console.log(that);
-// 			// relocate(that);
-
-// });
-// var knightStats = ["Bast", "knight", 250, 2, 50, 20, false, "knight.png"];
-// var mageStats = ["Denna", "mage", 100, 4, 25, 40, true, "mage.png"];
 
 var heroes = {
 	"knight": {
 		"name": "knight",
-		"hp": 350,
+		"hp": 250,
 		"attk": 4,
 		"def": 75,
 		"counterAttack": 20,
 		"state": "resting",
-		
+		"level": 1,	
 	},
 	"mage": {
 		"name": "mage",
@@ -133,7 +45,7 @@ var heroes = {
 		"def": 25,
 		"counterAttack": 40,
 		"state": "resting",
-		
+		"level": 1,	
 	},
 	"priest": {
 		"name": "priest",
@@ -142,7 +54,7 @@ var heroes = {
 		"def": 25,
 		"counterAttack": 40,
 		"state": "resting",
-		
+		"level": 1,	
 	},
 	"monk": {
 		"name": "monk",
@@ -151,7 +63,7 @@ var heroes = {
 		"def": 25,
 		"counterAttack": 40,
 		"state": "resting",
-		
+		"level": 1,	
 	},
 }
 
@@ -183,17 +95,13 @@ console.log("currentEnemy: " + currentEnemy.name);
 $(".hero").on("click", function() {
 
 	var clickedHero = $(this).attr("data-hero");
-	console.log("clickedHero: " + clickedHero);
 	var currHero = heroes[clickedHero];
-	console.log("currHero: " + currHero);
 	var heroPosition = $(this).attr("data-location");
-	console.log("heroPosition: " + heroPosition);
 
 	if (empty === true && heroPosition === "camp") {
 		empty = false;
 		move(this);
 		currHero.state = "battling";
-		console.log(currHero)
 		beginBattle(currHero);
 	}
 	else if ($(this).attr("data-location") === "battle") {
@@ -216,11 +124,10 @@ function move(image) {
 		$("#teamCamp").append(image);
 		$(image).attr("data-location", "camp");
 		$(image).removeClass("col-md-10 col-md-offset-2 battle").addClass("col-xs-3");
-		var image = $(image).children("h3");
 		hpIcon.addClass("campHp").removeClass("battleHp");
 	}
 	else {
-		alert("fail");
+		alert("move fail");
 	}
 }
 
@@ -229,9 +136,14 @@ function beginBattle(hero) {
 	var enemy = currentEnemy;
 	var player = hero;
 	$("#attackButton").on("click", function() {
+		var damageDone = player.attk * player.level;
+		currentEnemy.hp = currentEnemy.hp - damageDone;
+		console.log(player + currentEnemy.hp);
+		player.level++;
+		var damageTaken = currentEnemy.counterAttack;
+		player.hp = player.hp - damageTaken;
 		redraw(hero);
 	})
-
 }
 
 function endBattle() {
@@ -239,25 +151,18 @@ function endBattle() {
 }
 
 function redraw(character) {
-	console.log($(".battle"));
-	console.log($(".battle > h3"));
 	var hp = $(".battle > h3");
 	hp.text(character.hp);
+	var enemyHp = $("#enemyField > h3");
+	enemyHp.text(currentEnemy.hp);
 }
 
 function draw(characters) {
 	var charNames = ["priest", "mage", "monk", "knight"]
-	console.log(characters[charNames[2]]);
 	var hp = $(".camp > h3");
-	console.log(hp);
-	console.log(heroes.length);
 	for (var i = 0; i < 4; i++) {
-		// hp[i].text(characters[charNames[i]].hp);
-		console.log(hp[i]);
 		$(hp[i]).text(characters[charNames[i]].hp);
 	}
-	
-
 }
 
 draw(heroes);
